4c60ddb84d615bf161ea7db16883b42b5151e07f
M-e-r-c-y <mercyderese@gmail.com>
2024-05-31 18:40:17 -0700

    while loop

---
+++ 2_loops/cat_whileloop.py
@@ -0,0 +1,20 @@
+#while loop to excute Cat meow 3 times
+i = 0
+while i < 3:
+    print("meow")
+    i += 1          # i = i + 1
+
+'''
+#Option 2
+i = 3
+while i != 0:
+    print("meow")
+    i = i - 1
+'''
+'''
+#Option 3
+i = 1
+while i <= 3:
+    print("meow")
+    i = i + 1
+'''
